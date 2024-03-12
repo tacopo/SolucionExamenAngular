161661386fe216bc7e078e23d38ca165e1d6b2b1
CARLOS <charlicant@gmail.com>
2024-03-12 17:12:18 +0100

    añadir llamada a componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NavbarComponent} from "./componentes/navbar/navbar.component";
+import {HomeComponent} from "./pages/home/home.component";
+import {ObrasComponent} from "./pages/obras/obras.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, HomeComponent, ObrasComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {HomeComponent} from "./pages/home/home.component";
+import {ObrasComponent} from "./pages/obras/obras.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponent
+  },
+  {
+    path:"obras",
+    component: ObrasComponent
+  }
+
+];
